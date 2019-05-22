ba063ae27a854339b79515fecd8d3cd03119c559
Alexander Borovskikh <borislav_god@mail.ru>
2019-05-22 20:30:50 +0500

    home work

--- StickerLib.UI/Common/Services/DialogService.cs
+++ StickerLib.UI/Common/Services/DialogService.cs
@@ -1,14 +1,55 @@
-﻿namespace StickerLib.UI.Common.Services
+﻿using System;
+using System.Windows.Controls;
+
+namespace StickerLib.UI.Common.Services
 {
     public class DialogService : IDialog
     {
+        public void ShowInfo(string message)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void ShowSuccess(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowError(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowWarning(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ShowRequest(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowLoading(string message, Action callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowDialog(ItemsControl content, string title)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public interface IDialog
     {
-        void ShowInfo();
-        bool ShowRequest();
-        void ShowDialog();
+        void ShowInfo(string message);
+        void ShowSuccess(string message);
+        void ShowError(string message);
+        void ShowWarning(string message);
+        bool ShowRequest(string message);
+        void ShowLoading(string message, Action callback);
+
+        void ShowDialog(ItemsControl content, string title);
     }
 }
