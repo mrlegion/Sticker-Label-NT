ac4f3d09d46e4269b519e98b465f873cd8ea4c9f
Alexander Borovskikh <borislav_god@mail.ru>
2019-07-22 20:46:10 +0500

    1

--- StickerLib.UI/ViewModels/LibraryAddStickerListViewModel.cs
+++ StickerLib.UI/ViewModels/LibraryAddStickerListViewModel.cs
@@ -1,4 +1,7 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Microsoft.WindowsAPICodePack.Dialogs;
 using StickerLib.UI.Common.Services;
 
 namespace StickerLib.UI.ViewModels
@@ -12,6 +15,42 @@ namespace StickerLib.UI.ViewModels
             _dialog = dialog;
             _dialog.AlertDialogHost = "AlertLibraryDialogHost";
             _dialog.LoadingDialogHost = "LoadingLibraryDialogHost";
+        }
+
+        private RelayCommand<string> _selectFileCommand;
+
+        public RelayCommand<string> SelectFileCommand
+        {
+            get
+            {
+                return _selectFileCommand ?? (_selectFileCommand = new RelayCommand<string>((type) =>
+                {
+                    if (IsEmpty(type)) return;
+                    var dialog = OpenFileDialog("Select Titles file", false,
+                        new[] { new CommonFileDialogFilter("CVS file", "cvs") });
+
+
+                }));
+            }
+        }
+
+        private CommonOpenFileDialog OpenFileDialog(string title, bool multiselect, CommonFileDialogFilter[] filters)
+        {
+            var dialog = new CommonOpenFileDialog()
+            {
+                Title = title,
+                Multiselect = multiselect,
+            };
+
+            foreach (var filter in filters)
+                dialog.Filters.Add(filter);
+
+            return (dialog.ShowDialog() == CommonFileDialogResult.Ok) ? dialog : null;
+        }
+
+        private bool IsEmpty(string value)
+        {
+            return string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value);
         }
     }
 }
