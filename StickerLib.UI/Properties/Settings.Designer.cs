75f7a530318b095e7bc62dd8a973f8125f35a30f
Alexander Borovskikh <borislav_god@mail.ru>
2019-07-25 15:44:21 +0500

    adding new propery DialogShortTime

--- StickerLib.UI/Properties/Settings.Designer.cs
+++ StickerLib.UI/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace StickerLib.UI.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.2.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -200,6 +200,18 @@ namespace StickerLib.UI.Properties {
             }
             set {
                 this["TitleForApplication"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1000")]
+        public int ShortDialogDelay {
+            get {
+                return ((int)(this["ShortDialogDelay"]));
+            }
+            set {
+                this["ShortDialogDelay"] = value;
             }
         }
     }
