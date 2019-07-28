bfd0481d393a6ffa770f73f99a9cb4c7c1f54298
Alexander <borislav_god@mail.ru>
2019-07-28 20:31:10 +0500

    home work

--- StickerLib.Domain/Helpers/FileReaderHelper.cs
+++ StickerLib.Domain/Helpers/FileReaderHelper.cs
@@ -63,7 +63,7 @@ namespace StickerLib.Domain.Helpers
 
         private static char CreateDelimiter(Delimiter delimiter)
         {
-            char result = '';
+            char result = ';';
             switch (delimiter)
             {
                 case Delimiter.None:
@@ -82,48 +82,5 @@ namespace StickerLib.Domain.Helpers
             }
             return result;
         }
-
-        // maybe create string template for file
-        // example: {@title};{@page}
-        // or:      [$title$]-[$page$]
-
-        // Todo: create algoriphm to use read file use to template
-        //public static List<string> Read(string file, string template)
-        //{
-        //    // check file
-        //    if (!File.Exists(file))
-        //    {
-        //        Log.Fatal("Not found file with titles. File: [ {@file} ]", file);
-        //        throw new FileNotFoundException("Not found file with titles.", nameof(file));
-        //    }
-
-        //    //check pattern
-        //    if (string.IsNullOrEmpty(template) || string.IsNullOrWhiteSpace(template))
-        //    {
-        //        Log.Fatal("Template for read file cannot be nullable or empty");
-        //        throw new FileNotFoundException("Template for read file cannot be nullable or empty", nameof(template));
-        //    }
-
-        //    try
-        //    {
-        //        using (var sr = new StreamReader(file))
-        //        {
-        //            var result = new List<string>();
-        //            string line;
-        //            while ((line = sr.ReadLine()) != null)
-        //            {
-        //                result.Add(line);
-        //            }
-
-        //            return result;
-        //        }
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Console.WriteLine(e);
-        //        throw;
-        //    }
-        //    throw new NotImplementedException();
-        //}
     }
 }
